adc28659b56010c6d9c0508c4135c138d4534c80
fred <fredrickkisingo@gmail.com>
2019-07-30 00:52:19 +0300

    third commit

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,6 @@
 @section ('content')
       <h1>{{$title}}</h1>
       <p>
-        Under Construction
+        This a  website that aims to provide a platform where you get to find your healthy meal choices and be connected to the farmer .
       </p>
 @endsection
